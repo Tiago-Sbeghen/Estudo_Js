b26980dce7924e0576ecd859baa40eed9108ba20
Tiago-Sbeghen <tiago_cs_ferreira@estudante.sesisenai.org.br>
2026-03-18 14:34:30 -0300

    add - exercício - 2

--- exercicio_01.js
+++ exercicio_01.js
@@ -25,4 +25,4 @@ function maiorMenor(){
 
 };
 
-maiorMenor()
+//maiorMenor()
--- exercicio_02.js
+++ exercicio_02.js
@@ -1,7 +1,28 @@
 
 function mediaAcima(){
 
+    let valor = [];
+    let valor_acima = 0;
+    let media = 0.0;
+
+    for(let i = 0; i < 8; i++) {
+        valor[i] = Number(prompt("Informe um valor"));
+        media = media + valor[i];
+    };
+
+    media = media/8;
 
 
+    for(let i = 0; i < 8; i++) {
 
-}
+        if (valor[i] > media) {
+            valor_acima++;
+        }
+    };
+
+    console.log("Media: " + media);
+    console.log("Números Acima: " + valor_acima);
+
+};
+
+mediaAcima()
