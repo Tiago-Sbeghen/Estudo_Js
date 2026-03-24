3daa8ae1278fef3eed7218fe089eb04fef440390
Tiago-Sbeghen <tiago_cs_ferreira@estudante.sesisenai.org.br>
2026-03-24 08:00:20 -0300

    add - exercício 3

---
+++ exercicio_03.js
@@ -0,0 +1,27 @@
+
+function contadorParIpar(){
+
+    let numero = 0;
+    let pares = 0;
+    let impares = 0;
+
+    do{
+        
+        numero = Number(prompt("Informe um valor: [digite 0 para finalizar]"))
+
+        if(numero == 0){
+            break
+        } else if(numero % 2 == 0) {
+            pares++;  
+        } else {
+            impares++;
+        };
+
+    } while(numero != 0);
+
+    console.log(pares);
+    console.log(impares);
+
+};
+
+contadorParIpar()
