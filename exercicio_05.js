5dbfcb057c2468a4c6c82c842c7325bdea1b65af
Tiago-Sbeghen <tiago_cs_ferreira@estudante.sesisenai.org.br>
2026-03-25 07:53:36 -0300

    add - exercício - 5

---
+++ exercicio_05.js
@@ -0,0 +1,16 @@
+
+function tabuada(){
+
+    let numero = Number(prompt("Informe um número"));
+    let valor = 0;
+
+    for(let i = 0; i < 10; i++){
+        
+        valor = numero * (i+1);
+        console.log(valor);
+
+    };
+
+};
+
+tabuada()
