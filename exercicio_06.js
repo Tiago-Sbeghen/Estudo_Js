e066ca7d631f1e9168d8224622521b37726acf06
Tiago-Sbeghen <tiago_cs_ferreira@estudante.sesisenai.org.br>
2026-03-25 09:28:53 -0300

    add - exercício - 7

--- exercicio_06.js
+++ exercicio_06.js
@@ -21,4 +21,4 @@ function somaArray(){
 
 };
 
-somaArray()
+//somaArray()
---
+++ exercicio_07.js
@@ -0,0 +1,34 @@
+
+function ordenacaoSemSort() {
+
+    let valor = [];
+    let temp = 0;
+
+    for (let i = 0; i < 5; i++) {
+
+        valor[i] = Number(prompt("Informe um valor"))
+
+    };
+
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 5; j++) {
+
+            if (valor[j] > valor[j + 1]) {
+
+                temp = valor[j];
+                valor[j] = valor[j + 1];
+                valor[j + 1] = temp;
+
+            };
+        };
+    };
+
+    for(let i = 0; i < 5; i++){
+
+        console.log(valor[i]);
+
+    };
+
+};
+
+ordenacaoSemSort()
