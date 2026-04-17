337f063fad0f5fc19ca0dc624936acd0b58f4f6d
Tiago Cabral Sbeghen Ferreira <tiago_cs_ferreira@estudante.sesisenai.org.br>
2026-04-17 16:48:57 -0300

    up - alterei a formatação do documento, separei os exercícios que havia feito inicialmente em uma página e coloquei os exercícios novos em outra para uma melhor organização, conclui o exercício 1 módulo 2 apenas para aquecimento.

--- Exercício parte - 1/exercicio_08.js
+++ Exercício parte - 1/exercicio_08.js
@@ -24,4 +24,4 @@ function jogoAdivinhacao() {
 
 };
 
-jogoAdivinhacao()
+//jogoAdivinhacao()
---
+++ Exercício parte - 2/exercicio2_01.js
@@ -0,0 +1,12 @@
+
+function exercicio_01() {
+
+let numero = 0;
+
+numero = Number(prompt("Informe um número"));
+
+console.log("O número é " + numero);
+
+};
+
+//exercicio_01()
