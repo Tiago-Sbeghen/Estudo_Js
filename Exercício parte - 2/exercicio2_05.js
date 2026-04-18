a8b6478e105588b96f919b106c9191e417e5cc41
Tiago Cabral Sbeghen Ferreira <tiago_cs_ferreira@estudante.sesisenai.org.br>
2026-04-18 09:16:01 -0300

    add - mod 2, exercício 5

---
+++ Exercício parte - 2/exercicio2_05.js
@@ -0,0 +1,28 @@
+
+function exercicio_m2_05(){
+
+    let number_01;
+    let number_02;
+
+    let soma;
+    let sub;
+    let mult;
+    let div;
+
+
+    number_01 = Number(prompt("Informe um número"));
+    number_02 = Number(prompt("Informe outro número"));
+
+    soma = number_01 + number_02;
+    sub = number_01 - number_02;
+    mult = number_01 * number_02;
+    div = number_01 / number_02;
+
+    console.log("Soma: " + soma);
+    console.log("Subtração: " + sub);
+    console.log("Multiplicação: " + mult);
+    console.log("Divisão: " + div);
+
+};
+
+//exercicio_m2_05()
